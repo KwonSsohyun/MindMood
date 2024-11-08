4b7771db224360533016c5db5b93fd96e3962875
KwonSsohyun <thgus4982@naver.com>
2024-11-08 17:53:27 +0900

    ✨feat : .env API 응답 메시지 변수 분리 및 관리 개선

--- pages/api/loginUser.ts
+++ pages/api/loginUser.ts
@@ -24,7 +24,8 @@ const handler = async (req, res) => {
             // 비밀번호 확인
             if (!user) {
                 console.log('사용자를 찾을 수 없음');
-                return res.status(401).json({ message: '아이디 또는 비밀번호가 잘못되었습니다.' });
+                return res.status(401).json({ message: process.env.LOGINFAILEDMSG });
+                // return res.status(401).json({ message: '아이디 또는 비밀번호가 잘못되었습니다.' });
             }
 
             // console.log('사용자 조회 성공 : ', user);
